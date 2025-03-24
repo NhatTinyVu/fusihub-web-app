948f9cb3a0043b19c2e26368fba95f1c5840af7a
Nhat Vu <42594509+NhatTinyVu@users.noreply.github.com>
2025-03-24 21:28:10 +0700

    add tracing (#13)
    Co-authored-by: Nhat Tiny Vu <nhattinyvu@nhattinyvu-mb2019.local>

--- crates/services/web-backend-server/examples/quick_dev.rs
+++ crates/services/web-backend-server/examples/quick_dev.rs
@@ -7,7 +7,7 @@ pub type Error = Box<dyn std::error::Error>; // For examples.
 async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/index.html").await?.print().await?;
+    hc.do_get("/api/index.html").await?.print().await?;
 
     Ok(())
 }
--- crates/services/web-backend-server/src/main.rs
+++ crates/services/web-backend-server/src/main.rs
@@ -8,15 +8,22 @@ use lib_web::routes::routes_static;
 
 use axum::Router;
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .without_time() // For early local development.
+        .with_target(false)
+        .init();
+
     let routes_all =
         Router::new().fallback_service(routes_static::serve_dir(&web_config().STATIC_FILES_PATH));
 
     // region:    --- Start Server
     // Note: For this block, ok to unwrap.
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    info!("{:<12} - {:?}\n", "LISTENING", listener.local_addr());
     axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
