8c8d2f60c637a8313a50a9fd6a548265661b7af6
Nhat Tiny Vu <nhattinyvu@nhattinyvu-mb2019.local>
2025-03-30 18:25:01 +0700

    change backend port

--- crates/services/web-backend-server/src/main.rs
+++ crates/services/web-backend-server/src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> Result<()> {
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static::serve_dir(&web_config().STATIC_FILES_PATH));
 
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8888").await.unwrap();
     info!("{:<12} - {:?}\n", "LISTENING", listener.local_addr());
     axum::serve(listener, routes_all.into_make_service())
         .await
