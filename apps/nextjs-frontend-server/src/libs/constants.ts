ecd4a2688d67a11ebb249a1961ce7f8280a8648c
Nhat Tiny Vu <nhattinyvu@nhattinyvu-mb2019.local>
2025-04-07 12:26:47 +0700

    Update og data

--- apps/nextjs-frontend-server/src/libs/constants.ts
+++ apps/nextjs-frontend-server/src/libs/constants.ts
@@ -6,9 +6,9 @@ export const SITE_URL = isProduction
 
 export const GITHUB_USERNAME = "nhattinyvu";
 
-export const SITE_NAME = "FusiHub";
+export const SITE_NAME = "Fusion Hub";
 export const SITE_DESCRIPTION =
-  "A self-hosted, personal website built with Next.js, and Axum - Rust.";
+  "A personal self-hosted microservice website powered by Axum (Rust) and Next.js.";
 export const SITE_KEYWORDS = [
   "nhattinyvu",
   "Next.js",
